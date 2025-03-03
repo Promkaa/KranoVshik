9f41e7ac58947b2f8ff0f092f28c23fcc88712cf
ECLIPS12345 <165345268+ECLIPS12345@users.noreply.github.com>
2025-03-03 09:43:34 +0700

    Тест

--- Kran.py
+++ Kran.py
@@ -13,7 +13,7 @@ pygame.display.set_icon(icon)
 
 # Настройки экрана
 SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 1000
+SCREEN_HEIGHT = 800
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Крановщик")
 
