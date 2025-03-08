4e8a0573b61f661235cdc18d7f1b73bf2c5e6b9d
ECLIPS12345 <165345268+ECLIPS12345@users.noreply.github.com>
2025-03-08 15:44:02 +0700

    Доработал button.py

--- button.py
+++ button.py
@@ -1,7 +1,6 @@
 import pygame
-
 class ImageButton:
-    def _init_(self, x, y, width, height, image_path, hover_image_path = None, sound_path = None):
+    def __init__(self, x, y, width, height, image_path, hover_image_path=None, sound_path=None):
         self.x = x
         self.y = y
         self.width = width
@@ -14,11 +13,9 @@ class ImageButton:
         if hover_image_path:
             self.hover_image = pygame.image.load(hover_image_path)
             self.hover_image = pygame.transform.scale(self.hover_image, (width, height))
-        self.rect = self.image.get_rect(topleft=(x,y))
-        self.sound = None
 
-        if sound_path:
-            self.sound=pygame.mixer.Sound(sound_path)
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.sound = pygame.mixer.Sound(sound_path) if sound_path else None
         self.is_hovered = False
 
     def draw(self, screen):
@@ -29,7 +26,8 @@ class ImageButton:
         self.is_hovered = self.rect.collidepoint(mouse_pos)
 
     def handle_event(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.is_hovered:
-            if self.sound:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if self.is_hovered and self.sound:
                 self.sound.play()
-            pygame.event.post(pygame.event.Event(pygame.USEREVENT, button=self))
+            if self.is_hovered:
+                pygame.event.post(pygame.event.Event(pygame.USEREVENT, {'button': self}))
